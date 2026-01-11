f7769039c9b8091f6bef59f0a04716c089f818ce
Christos Feizidis <c.feizidis@gmail.com>
2026-01-11 14:59:37 +0200

    change the cron time, implement an index.json file with data like an API endpoint

--- super.py
+++ super.py
@@ -4,10 +4,38 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from PIL import Image
 from io import BytesIO
+from dataclasses import dataclass, asdict
+from typing import List
+import json
 
 BASE = "https://www.fylladiomat.gr"
+BASE_URL_JSON = "https://feizidischristos.github.io/super/flyers/"
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 OUTPUT_DIR = "flyers"
+
+@dataclass
+class Retailer:
+    name: str
+    logo: str
+    catalog: str
+
+    def to_dict(self):
+        return asdict(self)
+
+@dataclass
+class CatalogIndex:
+    base_url: str
+    retailers: List[Retailer]
+
+    def save(self, path: str):
+        payload = {
+            "base_url": self.base_url,
+            "retailers": [r.to_dict() for r in self.retailers]
+        }
+
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(payload, f, indent=4, ensure_ascii=False)
+
 
 BROCHURES_NAME_DIC = {"Bazaar": "bazaar",
              "Discount Markt": "discount_markt",
@@ -45,10 +73,10 @@ def get_brands():
             for cat in a.find_all("li"):
                 name = cat.get_text(strip=True)
                 href = cat.find("a").get("href")
-                
+
                 if name:
                     brands[name] = urljoin(BASE, href)
-                    
+
 
     return brands
 
@@ -59,8 +87,8 @@ def get_brochures(brand_url):
     )
 
     brochures = []
-    
-    
+
+
     for g in soup.body.main.find("div", class_="container shop-page"):
 #        print("\n\n")
         i = g.find("div", class_="row").find("div", class_="row").find("div", class_="brochure-thumb").find("a").get("href")
@@ -69,14 +97,25 @@ def get_brochures(brand_url):
 #        print("\n")
 
     return list(dict.fromkeys(brochures))
-    
+
+def get_logo(brand_url):
+    soup = BeautifulSoup(
+        requests.get(brand_url, headers=HEADERS).text,
+        "html.parser"
+    )
+
+    for g in soup.body.main.find("div", class_="frame"):
+        i = g.get("src").split(".png?",1)[0] + (".png")
+
+    return i
+
 def get_brochure_images(brochure_url):
-    
+
     soup = BeautifulSoup(
         requests.get(brochure_url, headers=HEADERS).text,
         "html.parser"
     )
-    
+
     images = []
     totalNumPages = 0
     for nu in soup.find_all("a", class_ = "btn btn-default page-num btn-sm navigate-brochure"):
@@ -86,7 +125,7 @@ def get_brochure_images(brochure_url):
     brochure_url = urljoin(brochure_url, "?page=")
 #    print(brochure_url)
 #    print("\n")
-    
+
     for p in range(1,int(totalNumPages)):
         soup = BeautifulSoup(
             requests.get(brochure_url + str(p), headers=HEADERS).text,
@@ -97,7 +136,7 @@ def get_brochure_images(brochure_url):
             img = pic.get("src")
 #            print(img)
             images.append(img)
-    
+
 
 #    # Remove duplicates, keep order
     seen = set()
@@ -133,13 +172,28 @@ def create_brand_pdf(brand_name, image_urls):
 def main():
     brands = get_brands()
 
+    retailers = []
+
     for brand, brand_url in brands.items():
         print(f"\n Processing brand: {brand}, {brand_url}")
 
         all_images = []
 
         brochures = get_brochures(brand_url)
+
         print(f" Found {len(brochures)} brochures")
+
+        brochureName  = BROCHURES_NAME_DIC[brand]
+
+        logoUrl = get_logo(brand_url)
+
+        retailers.append(
+            Retailer(
+                name=brand,
+                logo=logoUrl,
+                catalog=f"{brochureName}.pdf"
+            )
+        )
 
         for brochure in brochures:
             imgs = get_brochure_images(brochure)
@@ -147,9 +201,19 @@ def main():
             all_images.extend(imgs)
 
         if all_images:
-            create_brand_pdf(BROCHURES_NAME_DIC[brand], all_images)
+            create_brand_pdf(brochureName, all_images)
         else:
             print("No images found")
+
+    print("🧾 Generating index.json")
+
+    index = CatalogIndex(
+        base_url=BASE_URL_JSON,
+        retailers=retailers
+    )
+
+    index.save("flyers/index.json")
+
 
 if __name__ == "__main__":
     main()
